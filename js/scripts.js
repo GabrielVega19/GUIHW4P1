00cb73dc7d5a51ca9337c15227eed581faa5d331
gabriel <vegabito1112@gmail.com>
2022-11-25 19:09:16 -0500

    final

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,144 @@
-//Gets the elements we need for the scrips
-const myForm = document.querySelector("#myForm");
-const x1 = document.querySelector("#x1");
-const x2 = document.querySelector("#x2");
-const y1 = document.querySelector("#y1");
-const y2 = document.querySelector("#y2");
-const error = document.querySelector("#errors");
-const myTable = document.querySelector("#table");
-const unhide = document.querySelector("#unhide");
+// GUI Assignment: HW4
+// Gabriel Vega, UMass Lowell Computer Science, gabriel_vega@student.uml.edu
 
-//adds the function to be executed on the form when submit is pressed 
-myForm.addEventListener("submit", onSubmit);
+$(function() {
+    //validates all the input for the form and attaches the function to be used on valid submit
+    $("#myForm").validate({
+        rules: {
+            x1: {
+                required: true,
+                range: [-50, 50],
+                number: true
+            }, 
+            x2: {
+                required: true,
+                range: [-50, 50],
+                number: true,
+                gteqx: true
+            }, 
+            y1: {
+                required: true,
+                range: [-50, 50],
+                number: true
+            }, 
+            y2: {
+                required: true,
+                range: [-50, 50],
+                number: true,
+                gteqy: true
+            }
+        }, 
+        //sets error messages for every invalid input in the form
+        messages: {
+            x1: {
+                required: "Please enter a number for this field",
+                range: "Please enter a number between -50 and 50"
+            }, 
+            x2: {
+                required: "Please enter a number for this field",
+                range: "Please enter a number between -50 and 50",
+            }, 
+            y1: {
+                required: "Please enter a number for this field",
+                range: "Please enter a number between -50 and 50"
+            }, 
+            y2: {
+                required: "Please enter a number for this field",
+                range: "Please enter a number between -50 and 50",
+            }
+        },
+        submitHandler: function(form, e) {
+            //prevents the default behavior when the submit button gets pressed 
+            e.preventDefault();
 
-//function that gets rid of any children of an element
-function deleteChildren(myTable) {    
-    let child = myTable.lastElementChild; 
-    while (child) {
-        myTable.removeChild(child);
-        child = myTable.lastElementChild;
-    }
-}
+            //calculates the length of the x and y arrays 
+            const xLen = ($("#x2").val() - $("#x1").val()) + 1;
+            const yLen = ($("#y2").val() - $("#y1").val()) + 1;
 
-//function that executes when submit gets pressed 
-function onSubmit(e){
-    //prevents the default behavior when the submit button gets pressed 
-    e.preventDefault();
+            //unhides the table
+            $("#unhide").removeClass("hidden");
 
-    //calculates the length of the arrays 
-    const xLen = (x2.value - x1.value) + 1;
-    const yLen = (y2.value - y1.value) + 1;
+            //generates the multiplication table and axis headers
+            const xValues = new Array(xLen);
+            const yValues = new Array(yLen);
+            const multTable = new Array(yLen);
 
-    //makes sure the input was valid and if not outputs to error
-    if (xLen <= 0){
-        error.textContent = "Make sure X End is greater than or equal to X Start";
-        error.classList.remove("hidden");
-        return;
-    }
-    if (yLen <= 0){
-        error.textContent = "Make sure Y End is greater than or equal to Y Start";
-        error.classList.remove("hidden");
-        return;
-    }
-    //clears any lingering error messages 
-    error.classList.add("hidden");
-    //unhides the table
-    unhide.classList.remove("hidden");
+            //sets the values for the x and y headers
+            for (let i = 0; i < xLen; i++){
+                xValues[i] = parseInt($("#x1").val()) + i;
+            }
+            for (let i = 0; i < yLen; i++){
+                yValues[i] = parseInt($("#y1").val()) + i;
+            }
 
-    //generates the multiplication table and axis headers
-    const xValues = new Array(xLen);
-    const yValues = new Array(yLen);
-    const multTable = new Array(yLen);
+            for (let y = 0; y < yLen; y++){
+                multTable[y] = Array(xLen);
+                for (let x = 0; x < xLen; x++){
+                    multTable[y][x] = xValues[x] * yValues[y];
+                }
+            }
 
-    for (let i = 0; i < xLen; i++){
-        xValues[i] = parseInt(x1.value) + i;
-    }
-    for (let i = 0; i < yLen; i++){
-        yValues[i] = parseInt(y1.value) + i;
-    }
+            //clears any old data in the table
+            $("#table").html("");
 
-    for (let y = 0; y < yLen; y++){
-        multTable[y] = Array(xLen);
-        for (let x = 0; x < xLen; x++){
-            multTable[y][x] = xValues[x] * yValues[y];
-        }
-    }
+            //appends the top header to the table
+            let tr = $("<tr>");
+            console.log(tr);
+            for (let i = 0; i < (xLen + 1); i++) {
+                if (i == 0){
+                    tr.append($("<th>"));
+                } else{
+                    tr.append(`<th>${xValues[i-1]}</th>`);
+                }
+            }
+            $("#table").append(tr);
 
-    //clears any old data in the table
-    deleteChildren(myTable);
-
-    //appends top header
-    let tr = document.createElement("tr");
-    for (let i = 0; i < (xLen + 1); i++) {
-        const th = document.createElement("th");
-        if (i == 0){
-            tr.appendChild(th);
-        } else{
-            const textNode = document.createTextNode(xValues[i-1]);
-            th.appendChild(textNode);
-            tr.appendChild(th);
-        }
-    }
-    myTable.appendChild(tr);
-
-    //appends rest of table
-    for (let y = 0; y < yLen; y++){
-        tr = document.createElement("tr");
-        for (let x = 0; x < (xLen + 1); x++){
-            if (x == 0){
-                const th = document.createElement("th");
-                const textNode = document.createTextNode(yValues[y]);
-                th.appendChild(textNode);
-                tr.appendChild(th);
-            } else{
-                const td = document.createElement("td");
-                const textNode = document.createTextNode(multTable[y][x-1]);
-                td.appendChild(textNode);
-                tr.appendChild(td);
+            //appends rest of table
+            for (let y = 0; y < yLen; y++){
+                tr = $("<tr>");
+                for (let x = 0; x < (xLen + 1); x++){
+                    if (x == 0){
+                        tr.append(`<th>${yValues[y]}</th>`);
+                    } else{
+                        tr.append(`<td>${multTable[y][x-1]}</td>`);
+                    }
+                }
+                $("#table").append(tr);
             }
         }
-        myTable.appendChild(tr);
-    }
-}
+    });
+});
 
+//creates new validator to make sure that the xend is greater or equal to xstart 
+jQuery.validator.addMethod(
+    "gteqx", 
+    function(value, element) {
+        const xLen = ($("#x2").val() - $("#x1").val()) + 1;
+
+        //makes sure the make sure the second value was greater than the first one 
+        if (xLen <= 0){
+            return false;
+        }
+        else{
+            return true
+        }
+    }, 
+    "Make sure X End is greater than or equal to X Start"
+);
+
+//creates new validator to make sure that the yend is greater or equal to ystart 
+jQuery.validator.addMethod(
+    "gteqy", 
+    function(value, element) {
+        const yLen = ($("#y2").val() - $("#y1").val()) + 1;
+
+        //makes sure the make sure the second value was greater than the first one 
+        if (yLen <= 0){
+            return false;
+        }
+        else{
+            return true
+        }
+    }, 
+    "Make sure Y End is greater than or equal to Y Start"
+);
